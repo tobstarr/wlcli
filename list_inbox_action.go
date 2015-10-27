59965909c85f0e4bd155083b2b950f11f4dd57ad
Tobias Schwab <tobias.schwab@dynport.de>
2015-10-27 12:30:50 +0100

    allow filtering inbox via tag

--- list_inbox_action.go
+++ list_inbox_action.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strings"
 
 	"github.com/tobstarr/wlcli/Godeps/_workspace/src/github.com/dynport/gocli"
 )
 
 type listInboxAction struct {
+	Tag string `cli:"opt --tag"`
 }
 
 func (r *listInboxAction) Run() error {
@@ -27,8 +29,21 @@ func (r *listInboxAction) Run() error {
 	sort.Sort(tasks)
 	t := gocli.NewTable()
 	for _, task := range tasks {
+		if r.Tag != "" && !matchesTag(task.Title, r.Tag) {
+			continue
+		}
 		t.Add(task.CreatedAt.Format("2006-02-01 15:04:05"), task.ID, task.Title)
 	}
 	fmt.Println(t)
 	return nil
+}
+
+func matchesTag(text, tag string) bool {
+	tag = strings.ToLower(tag)
+	for _, f := range strings.Fields(strings.ToLower(text)) {
+		if f == "#"+tag {
+			return true
+		}
+	}
+	return false
 }
